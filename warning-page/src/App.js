8937ee380fa7628d1a693fcfb8c4211834cef0a6
kairi ode <kaisan.o@icloud.com>
2023-05-30 14:39:19 +0900

    create/warning/page

---
+++ warning-page/src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './App.css';
+// import date from '.date.jpg';
+
+
+function App() {
+  return (
+    <div>
+    <div className='flame'>
+      あなたのログイン情報は盗み取られています！！
+    <p className='underarrow'>↓↓↓</p>
+    </div>
+    <div>
+    {/* <img src={date} alt="date" /> */}
+  </div>
+  </div>
+    
+  );
+}
+
+export default App;
